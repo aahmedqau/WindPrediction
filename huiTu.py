9f2f4638db1c212c1eaf9165d8e85438b99f2756
Adeel Ahmed <106480940+aahmedqau@users.noreply.github.com>
2025-06-09 12:27:35 +0500

    Update huiTu.py

--- huiTu.py
+++ huiTu.py
@@ -1,22 +1,22 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# 读取CSV文件
+# Read the CSV file
 data = pd.read_csv('./Data/Five_year_2019_01_01-17_h_data.csv')
 
-# 获取第二列和第三列的数据
+# Get data from the second and third columns
 column2 = data.iloc[:, 1]
 column3 = data.iloc[:, 2]
 
-# 绘制图形
+# Plot the data
 plt.plot(column2, label='Column 2', color='blue')
 plt.plot(column3, label='Column 3', color='red')
 
-# 添加标签和标题
+# Add labels and title
 plt.xlabel('Row Index')
 plt.ylabel('Value')
 plt.title('Plot of Column 2 and Column 3')
 plt.legend()
 
-# 显示图形
+# Display the plot
 plt.show()
